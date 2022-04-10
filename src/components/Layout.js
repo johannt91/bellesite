7ab28bc05d205c7dce47980c021899555db521a7
Johann Taylor <j.taylor1343@gmail.com>
2022-04-10 18:41:02 -0400

    removed bootstrap and updated navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "@popperjs/core/dist/umd/popper.min.js";
 import Navbar from './Navbar'
 import Footer from './Footer'
 import '../assets/styles/global.css'
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,25 +2,21 @@ import React from "react";
 import { Link } from 'gatsby'
 import { StaticImage } from "gatsby-plugin-image"
 
-import { Navbar, Nav } from "react-bootstrap";
 
 const Navigation = () => {
   return (
     <header className="header">
-    
-      <Navbar collapseOnSelect expand="md" className="px-4 navbar fixed-top bg-white">
-        <Navbar.Brand><StaticImage src="../assets/images/logo.jpg" alt="Belles Artz" className="nav-logo"/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link exact activeClassName="active" to="/" as={Link} eventKey="1">About</Nav.Link>
-            <Nav.Link activeClassName="active" to="/portfolio" as={Link} eventKey="2">Brand portfolio</Nav.Link>
-            <Nav.Link activeClassName="active" to="/services" as={Link} eventKey="3">Services</Nav.Link>
-            <Nav.Link activeClassName="active" to="/contact" as={Link} eventKey="4">Contact Us</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
+      <nav className="navigation">
+        <div>
+          <StaticImage src="../assets/images/logo.jpg" alt="Belles Artz" className="nav-logo"/>
+        </div>
+          <ul className="nav-items">
+            <li className="nav-item"><Link exact activeClassName="active" to="/"  eventKey="1">About</Link></li>
+            <li className="nav-item"><Link activeClassName="active" to="/portfolio" as={Link} eventKey="2">Brand Portfolio</Link></li>
+            <li className="nav-item"><Link activeClassName="active" to="/services" as={Link} eventKey="3">Services</Link></li>
+            <li className="nav-item"><Link activeClassName="active" to="/contact" as={Link} eventKey="4">Contact Us</Link></li>
+          </ul>
+      </nav>
     </header>
   );
 };

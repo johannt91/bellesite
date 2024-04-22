7fd6cebcb471fa26ee84266f72d12ff190f1e039
Johann Taylor <j.taylor1343@gmail.com>
2024-04-22 12:16:22 -0400

    Added redirect to error page for empty pages

---
+++ src/pages/logoDesign.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ErrorPage from "./404"
+import Layout from '../components/Layout'
+
+const quote = () => {
+  return (
+    <Layout>
+        <ErrorPage/>
+    </Layout>
+  )
+}
+
+export default quote
